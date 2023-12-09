5b55c8d2a5a0f71746cb1a293110dc664144bc8b
Aniruddha Chandra <aniruddha.282003@gmail.com>
2023-12-09 14:31:09 +0530

    add all package

--- JS.js
+++ JS.js
@@ -1,3 +1,14 @@
+var _ = require('lodash');
+// console.log(_);
+// The browser does not know the import 
+// so we need to browsefy it 
+// install browserfy by the help of npm
+// Using browseify it convart out code into understandable for browser
+//  browserify JS.js > bun.js
+// using this comment we got a new js file which we need to link with main index.html file then only we we will get our result 
+var arr=[4,7,8,9,3];
+console.log(_.without(arr,3));
+// again we need to update the bun.js file in same prosses
 var body = document.querySelector("body");
 var color1=document.getElementById("color1");
 var color2=document.getElementById("color2");
